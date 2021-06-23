146941e5d2fce77d9489e38a4dabccd1912c3722
Ben <benjatubbs@gmail.com>
2021-06-23 16:43:19 -0700

    Updateing website

--- flask_app/dash/routes.py
+++ flask_app/dash/routes.py
@@ -12,11 +12,11 @@ def project():
 
 @app.route('/data')
 def data():
-	return render_template('data.html', title='data')
+	return render_template('data.html', title='Data')
 
 @app.route('/database')
 def database():
-	return render_template('database.html', title='database')
+	return render_template('database.html', title='Database')
 
 @app.route('/preprocessing', methods=['GET', 'POST'])
 def preprocessing():
@@ -26,7 +26,7 @@ def preprocessing():
 		classification = create_new_feature_set()
 		messages = ["A new dataset has been successfully generated."]
 		return redirect(url_for('preprocessing'))
-	return render_template('preprocessing.html', form=form, messages=messages, title='preprocessing')
+	return render_template('preprocessing.html', form=form, messages=messages, title='Preprocessing')
 
 @app.route('/model')
 def model():
@@ -53,8 +53,12 @@ def demo():
 		#message = Markup(f"<h3>You typed: { form.user_review.data }! <br /> This was classified as a <strong>{ classification }</strong> review.</h3>")
 		#flash(message, "CLASS_NAME")
 		return redirect(url_for('demo'))
-	return render_template('demo.html', title='Demo', form=form, messages=messages)
+	return render_template('demo.html', form=form, messages=messages, title='Demo')
 
 @app.route('/visualizations')
 def visualizations():
 	return render_template('visualizations.html', title='Visualizations')
+
+@app.route('/presentation')
+def presentation():
+	return render_template('presentation.html', title='Presentation')
